681373662264b568f0003f57442e6ca9fcb61828
hiroshi nagao <hi_1205@hotmail.com>
2024-03-24 12:16:24 +0900

    finish edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,11 +13,23 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+      if @article.save
+        redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
 
-    if @article.save
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
       redirect_to @article
     else
-      render :new, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
